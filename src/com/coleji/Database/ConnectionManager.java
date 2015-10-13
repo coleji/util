928234c78d46cc4cb29198831bdcc3fc30a831e4
jcole <coleji@bc.edu>
2015-10-13 15:29:10 -0400

    cvgjgfhj

--- src/com/coleji/Database/ConnectionManager.java
+++ src/com/coleji/Database/ConnectionManager.java
@@ -3,6 +3,7 @@ package com.coleji.Database;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+
 // TODO: (maybe?)  rather than create new connection on call to getConnection(), hand out connections from a pool of 2-3
 // maybe a different constructor that can do this?  Or two different manager subclasses?
 
